db1a41a7719a32d3d73a145cd6032173ee1135dd
FaizBShah <faizbshah2001@gmail.com>
2024-01-16 20:11:47 +0530

    Created the UI for the chip input

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -1,9 +1,16 @@
 import { FC } from 'react';
+import { FaTimes } from 'react-icons/fa';
 import { ChipProps as Props } from './Chip.types';
 import './Chip.scss';
 
-const Chip: FC<Props> = () => {
-  return <div>Chip</div>;
+const Chip: FC<Props> = ({ name, image }) => {
+  return (
+    <div className="chip">
+      <img className="chip-img" src={image} alt={name} />
+      <p>{name}</p>
+      <FaTimes className="chip-cancel-btn" />
+    </div>
+  );
 };
 
 export default Chip;
--- src/components/ChipInput/ChipInput.tsx
+++ src/components/ChipInput/ChipInput.tsx
@@ -1,10 +1,14 @@
 import { FC } from 'react';
 import './ChipInput.scss';
 import { ChipInputProps as Props } from './ChipInput.types';
+import Chip from '../Chip/Chip';
 
-const ChipInput: FC<Props> = ({ placeholder }) => {
+const ChipInput: FC<Props> = ({ placeholder, data }) => {
   return (
     <div className="chip-input-container">
+      {data.slice(0, 9).map((user, index) => (
+        <Chip key={index} name={user.name} image={user.image} />
+      ))}
       <input className="chip-input" placeholder={placeholder} />
     </div>
   );
